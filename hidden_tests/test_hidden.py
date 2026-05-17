402fb2e0e2e767b25897caf6440a7628e82aa3ee
RedaMehdaoui <mohamed.mehdaoui@epfl.ch>
2026-05-17 13:03:14 +0200

    Added tests that pretend to be hidden, and a summary

---
+++ hidden_tests/test_hidden.py
@@ -0,0 +1,4 @@
+from student_code.solution import add
+
+def test_large():
+    assert add(1000, 2000) == 3000
--- test_public.py
+++
@@ -1,7 +0,0 @@
-from student_code.solution import add
-
-def test_basic():
-    assert add(1, 2) == 3
-
-def test_negative():
-    assert add(-1, 1) == 0
